8afbf238cdb6d2e34f01123bcc0e075cc6e9fc05
Jedaigodx <thallisson.h.tp@gmail.com>
2025-08-08 22:21:31 -0300

    Corrigindo leitura da colunas do DataFrame

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd
 import re
 
-df = pd.read_csv('spam.csv', encoding='latin-1', sep='\t', header=None, names=['label', 'message'])
+df = pd.read_csv('spam.csv', encoding='latin-1')
 
+df = df[['v1','v2']]
+df.columns = ['label','message']
+
+print("Dados originais:")
 print(df.head())
 
 #função limpar limpar textos
 def clean_text(text):
     text = str(text).lower()
-    text = re.sub(r'http\S+|https\S+','',text)
+    text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
     text = re.sub(r'[^a-z0-9\s]', '', text)
     text = re.sub(r'\s+', ' ', text).strip()
     return text
@@ -17,5 +21,4 @@ def clean_text(text):
 df['clean'] = df['message'].apply(clean_text)
 
 print("\nExemplo de mensagem limpa:")
-
 print(df[['message', 'clean']].head())
